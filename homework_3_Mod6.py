79e875a009bedbbbd68cf159f6a721806eb99825
Hunt-Dim <166534559+Hunt-Dim@users.noreply.github.com>
2024-09-25 21:38:09 +0700

    Add files via upload

---
+++ homework_3_Mod6.py
@@ -0,0 +1,44 @@
+# Домашнее задание по теме "Множественное наследование"
+# Задача "Мифическое наследование":
+
+class Horse:
+    x_distance = 0  # пройденный путь
+    sound = 'Frrr'  # звук, который издаёт лошадь
+
+    def run(self, dx):
+       self.x_distance += dx
+       return self.x_distance
+
+
+class Eagle:
+    y_distance = 0  # высота полёта
+    sound = 'I train, eat, sleep, and repeat'  # звук, который издаёт орёл
+
+    def fly(self, dy):
+        self.y_distance += dy
+        return self.y_distance
+
+
+class Pegasus(Horse, Eagle):
+
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        tp1 = (self.x_distance, self.y_distance)
+        print(tp1)
+
+    def voice(self):
+        print(Eagle.sound)
+
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+p1.voice()
